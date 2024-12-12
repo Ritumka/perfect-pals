bfad2d87975723462bb909236873ed5fb2a20d3f
Margarita Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2024-12-12 05:02:39 +0000

    added ignored folders

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,22 @@
+import typescriptParser from "@typescript-eslint/parser";
+import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
+
 export default [
   {
     ignores: ["vendor/**", "node_modules/**"],
-    files: ["**/*.js"],
+    files: ["**/*.{js,ts}"],
     languageOptions: {
       ecmaVersion: "latest",
+      parser: typescriptParser, // Use the imported parser object
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslintPlugin
     },
     rules: {
       "no-unused-vars": "warn",
-      "no-console": "off"
+      "no-console": "off",
+      "@typescript-eslint/no-use-before-define": "off",
+      "@typescript-eslint/no-empty-function": "warn"
     },
   },
 ];
