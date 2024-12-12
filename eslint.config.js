9478e7f8bf094703a850ef247a15f9e1d447652d
Margarita Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2024-12-12 04:51:59 +0000

    added ignored folders

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 export default [
   {
-    ignores: ["vendor/javascript/**"],
+    ignores: ["vendor/**", "node_modules/**", "any_other_third_party_directories/**"],
     files: ["**/*.js"],
     languageOptions: {
       ecmaVersion: "latest",
